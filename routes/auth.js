5eafce681097d3a0a47782a7bbc2de79c14ace69
MaGi5ter <67414865+MaGi5ter@users.noreply.github.com>
2023-03-08 16:27:37 +0100

    login/register added

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,122 @@
 const express = require('express')
 const router = express.Router()
+const sha256 = require('sha256')
+let db = require('../mysql')  
 
 router
     .route("/:type")
-    .post((req,res,next) => {
+    .post(async (req,res,next) => {
         // LOGIN SCRIPT
-        console.log(req.params)
-
         if(req.params.type == 'login') {
-            res.send('login')
+
+            function redirect_message(msg) {
+                req.session.login_message = msg
+                res.redirect('/login')
+            }
+
+            console.log(req.body) // { email: '', password: '' }
+
+            let login = req.body.login
+            let password = req.body.password
+            
+            if(login == '' || password == '') redirect_message("Login/Password can't be empty")
+            
+            password = sha256(password)
+
+            if(login.includes("'") || login.includes(";")) redirect_message("something went wrong")
+
+            let row = await dbquery(`SELECT * FROM users WHERE login = '${login}' AND password = '${password}'`)
+
+            console.log(row)
+            if(row.length > 0) {
+
+                //here will be logged data
+                //jacapraca
+                res.redirect('/')
+
+            }
+            else redirect_message("password / login is wrong")
+
         } else next()
-
-
-    },(req,res) => {
+    },async (req,res) => {
         // REGISTER ACCOUNT SCRIPT
         if(req.params.type == 'register') {
-            res.send('register')
-        } else res.send('ERROR')
+
+            function redirect_message(msg) {
+                req.session.register_message = msg
+                res.redirect('/login')
+            }
+
+            if(req.body.login == '') {
+                redirect_message('No Login Provided')
+            }
+            else if (req.body.login.length > 20 || req.body.login.length < 4 ) {
+                redirect_message('Your login need to be beetween 4-20 characters')
+            }
+            else if(req.body.email == '') {
+                redirect_message('No email Provided')
+            }
+            else if(checkIfEmail(req.body.email) == false) {
+                redirect_message('This is not email')
+            }
+            else if(req.body.email > 254) {
+                redirect_message('This email is too Long')
+            }
+            else if(req.body.password[0] == '' || req.body.password[1] == '') {
+                redirect_message('No Password Provided')
+            }
+            else if(req.body.password[0] != req.body.password[1]) {
+                redirect_message('Passwords dont match')
+            }
+            else if(req.body.password[1] > 64) {
+                redirect_message('Your Password is too long')
+            }
+            // else if() {
+
+            // }
+            else {
+
+                let login = req.body.login
+                let email = req.body.email
+
+                if(login.includes("'") || email.includes("'")) return
+
+                let test_login = await dbquery(`SELECT * FROM users WHERE login = '${login}'`)
+                let test_email = await dbquery(`SELECT * FROM users WHERE email = '${email}'`)
+
+                if(test_login.length > 0 || test_email.length > 0) {
+                    redirect_message('This email or login is already used')
+                    return
+                }
+
+                let password = sha256(req.body.password[1])
+
+                console.log( login,email,password)
+
+                let sql = `INSERT INTO users (ID, login, email, password, privileges) VALUES (NULL, '${login}', '${email}', '${password}', '0');`
+
+                await dbquery(sql)
+
+                req.session.register_complete_message = 'Account Created You can LogIn now'
+
+                res.redirect('/login')
+            }
+
+        } else res.redirect('/')
     })
 
 module.exports = router
+
+function dbquery(prompt) {
+    return new Promise((resolve,reject) => {
+        db.query(prompt, function (err,rows){
+           if(err) reject(err)
+           else resolve(rows)
+        })
+    })
+}
+
+function checkIfEmail(str) {
+    const regexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+    return regexExp.test(str);
+  }
--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,12 @@ const router = express.Router()
 router
     .route("/")
     .get((req,res) => {
-        res.render('login', {} )
+
+        res.render('login', {
+            error_register : req.session.register_message,
+            error_login : req.session.login_message,
+            complete_register :req.session.register_complete_message,
+        } )
     })
 
 module.exports = router
